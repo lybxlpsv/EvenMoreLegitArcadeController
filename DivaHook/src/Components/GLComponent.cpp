ddb41f66d773e28ecfe34d97e6539d077086f3c3
lybxlpsv <lybxlpsv@yahoo.com>
2019-03-20 02:31:40 +0700

    make the game reset buttons in the same line

--- DivaHook/src/Components/GLComponent.cpp
+++ DivaHook/src/Components/GLComponent.cpp
@@ -190,8 +190,8 @@ namespace DivaHook::Components
 			ImGui::Begin("Reset Game", &showAbout, window_flags);
 			ImGui::SetWindowPos(ImVec2((hWindow.right / 2) - 100, (hWindow.bottom / 2) - 50));
 			ImGui::Text("Would you like to reset game?");
-			if (ImGui::Button("No")) { resetGameUi = false; };
-			if (ImGui::Button("Yes")) { resetGame = true; resetGameUi = false; showUi = false; showAbout = false; };
+			if (ImGui::Button("No")) { resetGameUi = false; }; ImGui::SameLine();
+			if (ImGui::Button("Yes")) { resetGame = true; resetGameUi = false; showUi = false; showAbout = false; }; ImGui::SameLine();
 			ImGui::End();
 		}
 
